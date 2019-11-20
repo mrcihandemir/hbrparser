08c408f28448d09cf8c25d0b7e38ed75bdcf6729
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-11-20 08:21:16 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+var cheerio = require('cheerio');
+let Parser = require('rss-parser');
+let parser = new Parser();
+const app = express(); 
+const port = process.env.PORT || 80
+
+
+app.use(express.static('/app'));
+
+app.get('/', function (req, res) {
+  // res.send('Hello World');
+  res.sendfile('index.html');
+  //res.send('hell');
+});
+
+app.listen(port, function(){
+  console.log("Listening port!")
+});
+
+
+
+/* 
+(async () => {
+ 
+  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
+ 
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
+ 
+})();
+*/
