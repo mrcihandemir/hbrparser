78506e3d205a2d58c9a6f142f428099d315654a3
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-11-25 13:49:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -54,10 +54,11 @@ app.get('/gen31', function (req, res) {
            let feed = await parser.parseURL(req.query.url);
            var result = [];    
            feed.items.forEach(item => {
-              
-              if (item.category !== 'Ekonomi') {      
+              if (item.category !== "Ekonomi") {      
               result.push({link: item.link, title: item.title, news: item.contentSnippet, img: item.enclosure.url, dt: item.isoDate  });
-              }      
+              }
+                    
+             
                     
                     
            });
