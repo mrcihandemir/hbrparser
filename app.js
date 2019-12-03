e5dbcb78a8a4a24596932a6d9e0d096a7b2af536
Cihan Demir <mrcihandemir@users.noreply.github.com>
2019-12-03 17:10:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -217,6 +217,7 @@ app.get('/gen140', function (req, res) {
 
 // Duvar Galeri
 app.get('/gal140', function (req, res) {
+    console.log(req.query.url);
     var $ = cheerio.load(req.query.url);
     console.log($('ul.swiper-wrapper.news-gallery').html());
     res.send($('meta[property="og:description"]').attr('content'));
